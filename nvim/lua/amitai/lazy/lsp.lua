2a1be3c50eb5ea60f6223c34631f87afd4430128
amitai-debiche <amivids603@gmail.com>
2025-03-29 21:33:14 -0500

    Added ruff to initial config

--- nvim/lua/amitai/lazy/lsp.lua
+++ nvim/lua/amitai/lazy/lsp.lua
@@ -23,6 +23,7 @@ return {
         require("mason").setup({})
         require("mason-lspconfig").setup({
             ensure_installed = {
+                "ruff",
                 "lua_ls",
                 "pyright",
             },
@@ -35,6 +36,21 @@ return {
 
             }
         })
+
+        require("lspconfig").pyright.setup {
+          settings = {
+            pyright = {
+              -- Using Ruff's import organizer
+              disableOrganizeImports = true,
+            },
+            python = {
+              analysis = {
+                -- Ignore all files for analysis to exclusively use Ruff for linting
+                ignore = { '*' },
+              },
+            },
+          },
+        }
 
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
@@ -57,6 +73,20 @@ return {
                 border = "rounded",
                 source = "always",
             },
+        })
+       vim.api.nvim_create_autocmd("LspAttach", {
+          group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+          callback = function(args)
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if client == nil then
+              return
+            end
+            if client.name == 'ruff' then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end,
+          desc = 'LSP: Disable hover capability from Ruff',
         })
     end
 }
